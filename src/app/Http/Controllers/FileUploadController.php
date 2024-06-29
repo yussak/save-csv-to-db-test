861abd5eb5cdb3bbb492f9fb5769010cc25d1d9c
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-06-29 12:11:27 +0900

    エラーハンドリング追加

--- src/app/Http/Controllers/FileUploadController.php
+++ src/app/Http/Controllers/FileUploadController.php
@@ -9,22 +9,29 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class FileUploadController extends Controller
 {
-    // TODO:いい感じのテスト用エクセルシート作る（分かる範囲で　複数シートにしたり）->とりあえずDONE
-    // TODO:DBにインポート可能にする->DONE
+    // いい感じのテスト用エクセルシート作る（分かる範囲で　複数シートにしたり）->とりあえずDONE
+    // DBにインポート可能にする->DONE
     // file not foundエラー出るので修正->DONE
-    // TODO:テスト用シートでDB migrateする->DONE
+    // テスト用シートでDB migrateする->DONE
     // ヘッダー無視->DONE
     // 列が25個と多いので対処。本来なら5→これ問題ないかもしれないな。とりあえずこの状態でインポートしてみる
-    // ファイル削除（必要なら）
+    // ファイル削除（必要なら）->現状必要ないので実装していない
     // 空のrowをskip->DONE
+    // エラーハンドリング追加　拡張子の分
+    // エラーハンドリング追加　その他の分
+    // フラッシュメッセージ出したい
 
     // エクセル、CSVどちらでやるか->ライブラリが使えるエクセルでいいと思う
 
     public function uploadFile(Request $request)
     {
         $file = $request->file("file");
+        if ($file->getClientOriginalExtension() !== "xlsx") {
+            throw new Exception(".xlsxのファイルをアップロードしてください");
+        }
+
         $fileName = $file->getClientOriginalName();
-        $file->storeAs("public", $fileName);
+
         try {
             Excel::import(new BooksImport, $file);
         } catch (Exception $e) {
