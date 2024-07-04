4066a7829044217a27786a9b52d02dca7df04a0f
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-07-05 00:08:52 +0900

    重複を弾くよう修正

--- src/app/Imports/BooksImport.php
+++ src/app/Imports/BooksImport.php
@@ -3,31 +3,38 @@
 namespace App\Imports;
 
 use App\Models\Book;
-use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\OnEachRow;
 use Maatwebsite\Excel\Concerns\WithStartRow;
+use Maatwebsite\Excel\Row;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-// SkipsEmptyRowsで空のrowをスキップ
-class BooksImport implements ToModel, WithStartRow, SkipsEmptyRows
+// 空行はこれでスキップされてそうだった
+class BooksImport implements OnEachRow, WithStartRow
 {
     /**
      * @param array $row
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-    public function model(array $row)
+    public function onRow(Row $row)
     {
-        return new Book([
-            // gettype(trim(44930))=>stringになる
-            "title" => $row[0],
-            "author" => $row[1],
-            "company" => $row[2],
-            // スペースはis_numeric=>falsenになるので、is_numeric内でtrimする必要はない
-            "published_at" => is_numeric($row[3]) ? Date::excelToDateTimeObject($row[3])->format("Y/m/d") : null,
-            "increased_at" => is_numeric($row[4]) ? Date::excelToDateTimeObject($row[4])->format("Y/m/d") : null,
-            "earnings" => $row[5],
-        ]);
+        // これがないとnot null violationが出る
+        if (!isset($row[0])) {
+            return null;
+        }
+        // すでにauthor_idがDBに入っているレコードと同じときはスキップ
+        return Book::firstOrCreate(
+            ["author_id" => $row[0]],
+            [
+                "author_id" => $row[0],
+                "title" => $row[1],
+                "author" => $row[2],
+                "company" => $row[3],
+                "published_at" => is_numeric($row[4]) ? Date::excelToDateTimeObject($row[4])->format("Y/m/d") : null,
+                "increased_at" => is_numeric($row[5]) ? Date::excelToDateTimeObject($row[5])->format("Y/m/d") : null,
+                "earnings" => $row[6],
+            ]
+        );
     }
 
     public function startRow(): int
