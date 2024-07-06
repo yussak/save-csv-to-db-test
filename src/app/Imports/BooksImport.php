171f5b4fbad33149e395fe386e36b3e1a183acb3
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-07-06 15:02:37 +0900

    重複チェックを修正 バッチインポート、空行スキップ追加

--- src/app/Imports/BooksImport.php
+++ src/app/Imports/BooksImport.php
@@ -3,42 +3,43 @@
 namespace App\Imports;
 
 use App\Models\Book;
-use Maatwebsite\Excel\Concerns\OnEachRow;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithStartRow;
-use Maatwebsite\Excel\Row;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-// 空行はこれでスキップされてそうだった
-class BooksImport implements OnEachRow, WithStartRow
+class BooksImport implements ToModel, WithStartRow, WithBatchInserts, SkipsEmptyRows
 {
     /**
      * @param array $row
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-    public function onRow(Row $row)
+    public function model(array $row)
     {
-        // これがないとnot null violationが出る
-        if (!isset($row[0])) {
-            return null;
-        }
-        // すでにauthor_idがDBに入っているレコードと同じときはスキップ
-        return Book::firstOrCreate(
-            ["author_id" => $row[0]],
-            [
-                "author_id" => $row[0],
-                "title" => $row[1],
-                "author" => $row[2],
-                "company" => $row[3],
-                "published_at" => is_numeric($row[4]) ? Date::excelToDateTimeObject($row[4])->format("Y/m/d") : null,
-                "increased_at" => is_numeric($row[5]) ? Date::excelToDateTimeObject($row[5])->format("Y/m/d") : null,
-                "earnings" => $row[6],
-            ]
-        );
+        $isExists = DB::table("books")->where("author_id", $row[0])->exists();
+        if ($isExists) return null;
+
+        return new Book([
+            "author_id" => $row[0],
+            "title" => $row[1],
+            "author" => $row[2],
+            "company" => $row[3],
+            "published_at" => is_numeric($row[4]) ? Date::excelToDateTimeObject($row[4])->format("Y/m/d") : null,
+            "increased_at" => is_numeric($row[5]) ? Date::excelToDateTimeObject($row[5])->format("Y/m/d") : null,
+            "earnings" => $row[6],
+        ]);
     }
 
     public function startRow(): int
     {
         return 2;
+    }
+
+    public function batchSize(): int
+    {
+        return 1000;
     }
 }
